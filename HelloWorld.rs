b970ef9a70b909a3286852ecc577069313330e5e
Charlotte <charlottejaconnolly+1@gmail.com>
2019-11-19 01:13:23 -0800

    Initial commit

---
+++ HelloWorld.rs
@@ -0,0 +1,17 @@
+print("Hello World!")
+
+
+; supports comments and empty lines
+
+print("Hello world again!")
+
+
+
+
+; Sorta... variables
+
+print("[#CRLF]Hello!")
+
+
+; And whitespace!
+      print("[#CRLF]Hello!")
